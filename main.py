8d5e9971fef8d7524e636e4a5bc19dc060504edc
tamir321 <48160658+tamir321@users.noreply.github.com>
2020-01-09 14:43:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ def start(args, hkubeapi):
     input=args['input'][0]
    
     time.sleep(input)
-    return {"name":"python test",
-            "version":"v2"}
+    return {"name":"python test from git",
+            "version":"v3"}
