cfae25c1512e57c1431acc2bb927e70c2e9d0d23
tamir321 <48160658+tamir321@users.noreply.github.com>
2020-03-18 10:32:44 +0200

    commit  A3

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ def start(args, hkubeapi):
    
     time.sleep(int(input))
     return {"name":"python test from git",
-            "commit":"A2",
+            "commit":"A3",
             "version":"master margev1"}
 
